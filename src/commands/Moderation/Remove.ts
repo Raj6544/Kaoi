f27bd5a2d02c21e8f5a1f974e145663a3acab939
Raj6544 <92223399+Raj6544@users.noreply.github.com>
2021-11-18 18:58:57 +0530

    Update Remove.ts

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             adminOnly: true,
-            aliases: ['boom', 'kick'],
+            aliases: ['boom'],
             command: 'remove',
             description: 'removes the mentioned users',
             category: 'moderation',
@@ -19,20 +19,20 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         let text = '*Action*\n\n'
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ Failed to ${this.config.command}, make me ADMIN first, baka`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
             // const usr = this.client.contacts[user]
             // const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
             if (M.groupMetadata?.owner.split('@')[0] === user.split('@')[0]) {
-                text += `❌ Skipped *@${user.split('@')[0]}* as they're owner.\n`
+                text += `*@${user.split('@')[0]}* is owner, baka.\n`
             }
             // check if user is Bot
             else if (this.client.user.jid === user) {
-                text += `❌ Skipped *@${user.split('@')[0]}* as they're me.\n`
+                text += `*@${user.split('@')[0]}* is me, baka.\n`
             } else {
-                text += `🟥 Removed *@${user.split('@')[0]}*\n`
+                text += `*@${user.split('@')[0]}* is removed 🧨\n`
                 await this.client.groupRemove(M.from, [user])
             }
         })
