ec64eeb80cfb1f706cb47769455b4a0d61c6a452
Raj6544 <92223399+Raj6544@users.noreply.github.com>
2021-11-18 19:00:33 +0530

    Update Promote.ts

--- src/commands/Moderation/Promote.ts
+++ src/commands/Moderation/Promote.ts
@@ -17,13 +17,13 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ Failed to ${this.config.command}, make me admin first, baka`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (M.groupMetadata?.admins?.includes(user)) M.reply(`*${username}* already an admin ಠಗಠ`)
+            if (M.groupMetadata?.admins?.includes(user)) M.reply(`*${username}* already an ADMIN ಠಗಠ`)
             else {
                 await this.client.groupMakeAdmin(M.from, [user])
                 M.reply(`*${username}* is an admin now 🎉`)
