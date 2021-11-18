9b60f66c65997ec64aad18a960fd88289d20f746
Raj6544 <92223399+Raj6544@users.noreply.github.com>
2021-11-18 19:04:28 +0530

    Update Demote.ts

--- src/commands/Moderation/Demote.ts
+++ src/commands/Moderation/Demote.ts
@@ -17,13 +17,13 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ Failed to ${this.config.command}/nmake me admin first, baka`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`*${username}* not an ADMIN ಠ∀ಠ`)
+            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`Skipped ‼️/n*${username}* not an ADMIN ಠ∀ಠ`)
             else if (user !== this.client.user.jid) {
                 await this.client.groupDemoteAdmin(M.from, [user])
                 M.reply(`*${username}* is not an ADMIN now ➰`)
